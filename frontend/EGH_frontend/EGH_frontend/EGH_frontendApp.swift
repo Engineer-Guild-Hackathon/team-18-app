f2832abcd96714c904b3a4efb84b05a4d812778a
tasotaso0503 <wantan.wantan@icloud.com>
2025-09-09 11:24:36 +0900

    first commit

---
+++ frontend/EGH_frontend/EGH_frontend/EGH_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  EGH_frontendApp.swift
+//  EGH_frontend
+//
+//  Created by 遠藤羊太郎 on 2025/09/09.
+//
+
+import SwiftUI
+
+@main
+struct EGH_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
